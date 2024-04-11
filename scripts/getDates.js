6789a0b0b0133db2e5dda18844ae2125e974efb0
Waldo Mauricio BC <112277562+WalMauBC@users.noreply.github.com>
2024-04-10 23:25:37 -0500

    W05

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,66 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lastModifiedDate = new Date(document.lastModified);
-    var footer = document.querySelector('footer');
-    var span = document.createElement('span');
-    span.textContent = "Last modified: " + lastModifiedDate.toLocaleString();
-    footer.appendChild(span);
+// Today´s Date
+const lastModified = document.querySelector('#lastModified');
+const lastStamp = document.querySelector('#timeStamp');
+var date = new Date(document.lastModified);
+var day = String(date.getDate()).padStart(2, '0');
+var month = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+var year = date.getFullYear();
+date = month + '/' + day + '/' + year;
+lastModified.textContent = date;
+// lastStamp.textContent = date;
+
+
+
+// navbar Code
+const hamButton = document.querySelector("#menu")
+const navigation = document.querySelector(".navigation");
+const visits = document.getElementById("visits")
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
 });
+
+//LocalStorage for the visits:
+let numOfVisits = Number(localStorage.getItem('Number of visits:'));
+
+if (numOfVisits !== 0){
+    visits.textContent = numOfVisits;
+}else{
+    visits.textContent = "Congrats you are the first visit 😁";
+}
+
+numOfVisits++;
+
+localStorage.setItem('Number of visits:', numOfVisits);
+
+//API constants and information :
+const api_key = 'f4020f49719cc815617a223c13df5681';
+const weather_div = document.querySelector("#weather_info"); 
+const icon = document.querySelector("#icon-weather");
+const temperature = document.querySelector("#temperature");
+const description = document.querySelector("#description-weather");
+
+async function fetchData(){
+    var cityname = document.querySelector("#city").value
+    if(cityname !==''){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api_key}&units=imperial`;
+    const response = await fetch(url);
+    if(response.ok){
+        const data = await response.json();
+        displayData(data);
+    }else{
+        console.log("error");
+    }
+}
+}
+function displayData(data){
+    temperature.innerHTML = `${data.main.temp} F`;
+    const icon_url =`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description
+    icon.setAttribute("src",icon_url);
+    icon.setAttribute("alt", desc);
+    description.innerHTML = `${desc}`;
+}
+
+document.querySelector("#getData").addEventListener('click', fetchData);
--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,36 @@
-document.addEventListener("DOMContentLoaded",(function(){const baseURL="",linksURL="data/links.json",ul=document.querySelector("#weeks-content");async function getLinks(){try{const response=await fetch(linksURL);if(!response.ok)throw Error(await response.text());{const data=undefined;displayLinks((await response.json()).weeks)}}catch(error){console.log(error)}}function displayLinks(weeks){weeks.forEach(week=>{let weekLi=document.createElement("li");weekLi.textContent=week.week+": ",ul.appendChild(weekLi),week.links.forEach((activity,index)=>{let aActivity=document.createElement("a");if(aActivity.textContent=activity.title+" ",aActivity.setAttribute("href",activity.url+" "),weekLi.append(aActivity),index<week.links.length-1){let separator=document.createElement("span");separator.textContent="|",weekLi.appendChild(separator)}})})}getLinks()}));
+const baseURL = "https://github.com/Zamarr0n/wdd230";
+
+const linksURL = "https://Zamarr0n.github.io/wdd230/data/links.json"
+
+
+async function getLinks(){
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+const list = document.getElementById("learning_activities_list");
+const displayLinks = (weeks) => {
+    weeks.forEach(weekData => {
+        var section = document.createElement('div');
+        var numWeek = document.createElement("li");
+
+        const week = weekData.week;
+        numWeek.textContent = `${weekData.week}: `;
+        var linksContainer = document.createElement('div'); 
+        weekData.links.forEach(link => {
+            var linkContent = document.createElement("a"); 
+            linkContent.setAttribute("href", `${link.url}`);
+            linkContent.textContent = ` ${link.title}`;
+            linkContent.classList.add("links")
+            linksContainer.appendChild(linkContent);
+            
+        });
+        numWeek.textContent = `${weekData.week}:`;
+        section.appendChild(numWeek);
+        section.appendChild(linksContainer); 
+        list.appendChild(section);
+    });
+}
+
+getLinks();
